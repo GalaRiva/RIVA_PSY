5f0c2ae270bef06987962f87dd757adde3ecbea3
plotnikov2281337 <plotnikofffromanchik@mail.ru>
2023-10-19 00:24:43 +0300

    add custom_tab_bar.dart

---
+++ lib/presentation/recomendation/recomendation_screen/working_out/models/spent_record_model.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'spent_record_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+SpentRecordModel _$SpentRecordModelFromJson(Map<String, dynamic> json) =>
+    SpentRecordModel(
+      dayEventModel:
+          DayEventModel.fromJson(json['dayEventModel'] as Map<String, dynamic>),
+      whyThisThoughts: json['whyThisThoughts'] as String,
+      alternativeThoughts: json['alternativeThoughts'] as String,
+      whyThisDo: json['whyThisDo'] as String,
+      alternativeDo: json['alternativeDo'] as String,
+    );
+
+Map<String, dynamic> _$SpentRecordModelToJson(SpentRecordModel instance) =>
+    <String, dynamic>{
+      'dayEventModel': instance.dayEventModel,
+      'whyThisThoughts': instance.whyThisThoughts,
+      'alternativeThoughts': instance.alternativeThoughts,
+      'whyThisDo': instance.whyThisDo,
+      'alternativeDo': instance.alternativeDo,
+    };
