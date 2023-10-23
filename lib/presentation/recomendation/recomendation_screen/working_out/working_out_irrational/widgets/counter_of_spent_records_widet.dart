690b456130c861f49618df44a1596e58152029b7
plotnikov2281337 <plotnikofffromanchik@mail.ru>
2023-10-23 14:57:30 +0300

    some changes in record_card.dart and expandeble_text_widget.dart. add count_bar

---
+++ lib/presentation/recomendation/recomendation_screen/working_out/working_out_irrational/widgets/counter_of_spent_records_widet.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CounterOfSpentRecordsWidget extends StatelessWidget {
+  const CounterOfSpentRecordsWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
